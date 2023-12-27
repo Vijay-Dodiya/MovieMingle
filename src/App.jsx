8777a409bc80109f36727054703da202ea9b0da3
Vijay <dodiyavijay047@gmail.com>
2023-12-27 16:42:17 +0530

    added 1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='App'> 
+      This is the app page which is i created for the movie site 
+    </div>
+  )
+}
+
+export default App
